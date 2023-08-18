2619ada33042a291765632084220d071cc299bbc
oi <oifuwe@luc.edu>
2023-08-18 07:45:22 -0500

    add timeout tests

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -2,8 +2,11 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.Exception.ValueNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,5 +30,19 @@ class IndexControllerTest {
   @DisplayName(value = "Test exception")
   void oupsHandler() {
     assertThrows(ValueNotFoundException.class, () -> controller.oopsHandler());
+  }
+
+  @Disabled(value = "Demo for timeout")
+  @Test
+  void testTimeout() {
+    assertTimeout(Duration.ofMillis(100), () -> Thread.sleep(2000));
+    System.out.println("I got here");
+  }
+
+  @Disabled(value = "Demo for timeout")
+  @Test
+  void testTimeoutPrempt() {
+    assertTimeoutPreemptively(Duration.ofMillis(100),() -> Thread.sleep(2000));
+    System.out.println("I got here in ....");
   }
 }
