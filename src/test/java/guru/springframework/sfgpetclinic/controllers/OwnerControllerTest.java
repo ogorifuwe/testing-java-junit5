190931385b6856ba5dae407b6f87956fb3e702ac
oi <oifuwe@luc.edu>
2023-08-20 09:30:15 -0500

    argument captor

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -5,12 +5,18 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +30,34 @@ class OwnerControllerTest {
 
   @InjectMocks
   private OwnerController controller;
+
+  @Captor
+  ArgumentCaptor<String> stringArgumentCaptor;
+
+  @Test
+  void processFindFormWildcardStringTest() {
+    List<Owner> owners = new ArrayList<>();
+    final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+    when(service.findAllByLastNameLike(captor.capture())).thenReturn(owners);
+
+    Owner owner = new Owner(1l, "James", "Hamilton");
+    String viewName = controller.processFindForm(owner, bindingResult, null);
+
+    verify(service).findAllByLastNameLike(captor.capture());
+    assertEquals("%Hamilton%", captor.getValue());
+  }
+
+  @Test
+  void processFindFormWildcardStringTestAnnotationBased() {
+    List<Owner> owners = new ArrayList<>();
+    when(service.findAllByLastNameLike(stringArgumentCaptor.capture())).thenReturn(owners);
+
+    Owner owner = new Owner(1l, "James", "Hamilton");
+    String viewName = controller.processFindForm(owner, bindingResult, null);
+
+    verify(service).findAllByLastNameLike(stringArgumentCaptor.capture());
+    assertEquals("%Hamilton%", stringArgumentCaptor.getValue());
+  }
 
   @Test
   void processCreationFormHasErrorsTest() {
